bd17af21a4f2bdc96a904338669728d5757ac3be
Facundo Pérez Duperré <facu6486@hotmail.com>
2022-03-29 00:43:33 -0300

    agregado navbar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return (
+        <header>
+            <nav>
+                <ul class="listaNav">
+                    {/* <li><a href="inicio.html">Inicio</a></li> */}
+                    <li><a href="#">Hamburguesas</a></li>
+                    <li><a href="#">Bebidas</a></li>
+                    <h1>BRD BURGER</h1>
+                    {/* <a href="#"><img className="logoNav" src="img/brd2.png" alt=""/></a> */}
+                    <li><a href="#">Contacto</a></li>
+                    <li><a href="#">Novedades</a></li>
+                    </ul>
+                </nav>
+        </header>
+    );
+}
+
+export default Navbar
